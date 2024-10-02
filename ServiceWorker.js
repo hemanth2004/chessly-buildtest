c29e027c472f91f3a815e7dc68845d713c208e13
hemanth2004 <hemanth.2004.elangovan@gmail.com>
2024-10-02 13:47:50 +0530

    tra

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,6 +1,33 @@
+const cacheName = "Hmnt-Chessly_WECREC-1.0";
+const contentToCache = [
+    "Build/chessly-buildtest.loader.js",
+    "Build/chessly-buildtest.framework.js",
+    "Build/chessly-buildtest.data",
+    "Build/chessly-buildtest.wasm",
+    "TemplateData/style.css"
+
+];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
 });
 
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
